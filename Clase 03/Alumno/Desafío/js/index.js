d37c9302b2bc3baf28aca1863502fa7fff5d95ce
juliherco <julieta.hercovich@gmail.com>
2019-04-21 11:26:25 -0300

    Added workshop HTML file

---
+++ Clase 03/Alumno/Desafío/js/index.js
@@ -0,0 +1,12 @@
+var operacion = prompt('Ingresá una operación')
+
+var parametro1 = prompt('Ingresá el primer parámetro')
+
+var parametro2 = prompt('Ingresá el segundo parámetro')
+
+do {
+  //TODO: Hacer
+} while (operacion === div && parametro2 === 0)
+{
+  promt('Volvé a ingresar el segundo parámetro')
+}
